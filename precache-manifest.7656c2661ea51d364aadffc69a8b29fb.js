4793c933fe9e7cb78034e8de763a01db49d542cc
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2019-11-15 10:11:02 -0500

    Updates

---
+++ precache-manifest.7656c2661ea51d364aadffc69a8b29fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50527488d210b36e5c7261706247d5d",
+    "url": "/monster_rolodex/index.html"
+  },
+  {
+    "revision": "5a96cf02bd052e10ed90",
+    "url": "/monster_rolodex/static/css/main.1627c5a7.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monster_rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "5a96cf02bd052e10ed90",
+    "url": "/monster_rolodex/static/js/main.f5036c69.chunk.js"
+  },
+  {
+    "revision": "7089f1d3336024d7315d",
+    "url": "/monster_rolodex/static/js/runtime-main.e5145b0c.js"
+  }
+]);
